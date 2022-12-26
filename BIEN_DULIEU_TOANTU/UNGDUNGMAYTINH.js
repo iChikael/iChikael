7e526eb7534ffe9fe439bf455683e412c3925c68
iChikael <120639771+iChikael@users.noreply.github.com>
2022-12-26 11:23:45 +0700

    BTT

---
+++ BIEN_DULIEU_TOANTU/UNGDUNGMAYTINH.js
@@ -0,0 +1,28 @@
+
+    function add() {
+        let number1 = document.getElementById("num1").value; 
+        let number2 = document.getElementById("num2").value; 
+        let result = Number(number1) + Number(number2); 
+        document.getElementById("result").innerHTML = result;
+    }
+
+    function minus() {
+        let number1 = document.getElementById("num1").value; 
+        let number2 = document.getElementById("num2").value; 
+        let result = Number(number1) - Number(number2); 
+        document.getElementById("result").innerHTML = result;
+    }
+    
+    function multi() {
+        let number1 = document.getElementById("num1").value; 
+        let number2 = document.getElementById("num2").value; 
+        let result = Number(number1) * Number(number2); 
+        document.getElementById("result").innerHTML = result;
+    }
+    
+    function device() {
+        let number1 = document.getElementById("num1").value; 
+        let number2 = document.getElementById("num2").value; 
+        let result = Number(number1) / Number(number2); 
+        document.getElementById("result").innerHTML = result;
+    }
