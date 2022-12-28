2ebdd6e60316da6cadc86dbb38001e6805bf0990
iChikael <120639771+iChikael@users.noreply.github.com>
2022-12-28 14:42:14 +0700

    BT

---
+++ FORBT.js
@@ -0,0 +1,41 @@
+//for ( let i=0 : i <= 100 ; i % 2 ==0 : i++ ){
+   // console.log(i);
+//}
+
+
+
+//for (let i = 0; i <= 100; i++) {
+//    if (i % 2 === 0) {
+//      console.log(i);
+//    }
+//  }
+//for ( var i = 0 ; i <=100 ; i +=2 ) {
+//    console.log(i);
+//}  
+//var i =1
+//while ( i <= 10){
+//    console.log(i); 
+//    i++;
+//}
+
+
+//var i = 1
+//var count =0
+//var number=7
+//while (i <= number){
+//    if(number % i ==0){
+//        count++;
+//    }
+//    i++
+//}
+//if (count==2){
+//    console.log("La so nguyen to");
+//}
+//else {
+//    console.log("Khong phai la so nguyen to");
+//}
+
+var i = 1
+var count =0
+var number=7
+
---
+++ foti.js
@@ -0,0 +1,196 @@
+// var n = 10
+// var x = 2
+// var s = 0
+
+
+// for(var i=1; i =<10; i++){
+//     s= s + x**i;
+// }
+// console.log(s);
+
+// var n = 10
+// var x = 3
+// var s = 0
+// for (var i =1 ; i <=10 ; i++){
+//     s = s + x**(2*i)
+
+// }
+  
+// console.log(s);
+//b1
+
+// var n =10
+// var s = 0
+// for (var i = 1; i<=10 ; i++){
+//     s = s + i
+// }
+// console.log(s);
+
+//b1.2
+
+// var n =10
+// var s =0
+// var i =1
+// do{
+//     s = s + i;
+//     i++;
+// }
+// while(i<=10);
+// console.log(s);
+
+//b1.3
+
+// var n =10
+// var s = 0
+// var i = 1
+// while (i<=10 ){
+//     s = s + i;
+//     i++
+// }
+// console.log(s);
+
+//b2
+
+// var n = 10
+// var s = 0
+// for (var i = 1 ; i <= 10 ; i++){
+//     s = s + i**2
+
+// }
+
+// console.log(s);
+
+//b2.1
+
+// var n = 10
+// var s = 0
+// var i = 1
+// do {
+//     s = s + i**2;
+//     i++
+// }
+// while (i<= 10)
+// console.log(s);
+
+//b2.2
+
+// var s=0
+// var i=1
+// while(i<=10){
+//     s = s + i**2;
+//     i++ 
+// }
+// console.log(s);
+
+//b3
+
+// var n = 10
+// var s = 0
+// for (var i = 1 ; i <= n ; i++){
+//     s = s + 1/i
+
+// }
+
+// console.log(s);
+
+//b3.1
+// var n = 10
+// var s = 0
+// var i = 1
+// do {
+//     s = s + 1/i;
+//     i++
+// }
+// while(i<=n)
+// console.log(s);
+
+//b3.2
+
+// var n = 10
+// var s = 0
+// var i = 1
+// while (i<=n){
+//     s= s + 1/i;
+//     i++
+// }
+// console.log(s);
+
+//b4
+// var s = 0
+// var n =20
+// for (var i = 1 ; i <= n ; i++){
+//     s = s + 1/(2*i)
+
+// }
+// console.log(s);
+
+//b4.1
+
+// var s = 0
+// var i = 1
+// var n = 20
+// while(i<=n){
+//     s = s + 1/(2*i);
+//     i++
+// }
+// console.log(s);
+
+//b4.2
+var s = 0 
+var i = 1
+var n = 20
+do {
+    s = s + 1/(2*i);
+    i++
+}
+while (i<=n);
+console.log(s);
+
+
+
+//b5
+// var s = 0
+// var n =10
+// for (var i = 1 ; i <= n ; i++){
+//     s = s + 1/(2*i+1)
+// }
+// console.log(s);
+
+//b6
+
+// var s = 0
+// var n =10
+// for (var i = 1 ; i <= n ; i++){
+//     s = s + 1/(i*(i+1))
+// }
+// console.log(s);
+
+//b7
+
+// var s = 0
+// var n =10
+// for (var i = 1 ; i <= n ; i++){
+//     s = s + i/(i+1)
+// }
+// console.log(s);
+
+//b8
+
+// var s = 0
+// var n =10
+// for (var i = 1 ; i <= n ; i++){
+//     s = s + 2*i+1/(2*i+2)
+// }
+// console.log(s);
+
+//b9
+
+//  var s = 1
+//  var n =10
+//  for (var i = 2 ; i <= n ; i++){
+//      s = s*i
+//  }
+//  console.log(s);
+
+
+
