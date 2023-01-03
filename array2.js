0c57849f4c0c75f7c71517e3a193ceea0cff789d
iChikael <120639771+iChikael@users.noreply.github.com>
2023-01-03 16:36:57 +0700

    BTT

--- array2.js
+++ array2.js
@@ -45,31 +45,49 @@
 // }
 
 // Câu 1. tính tổng giá trị đường viền: dùng càng ít vòng for càng tốt
-let arr = [];
-arr[0] = [2,3,4,5,7];
-arr[1] = [4,5,4,1,7];
-arr[2] = [2,1,6,2,7];
-arr[3] = [2,3,3,7,7];
-arr[4] = [1,5,4,7,9];
+// let arr = [];
+// arr[0] = [2,3,4,5,7];
+// arr[1] = [4,5,4,1,7];
+// arr[2] = [2,1,6,2,7];
+// arr[3] = [2,3,3,7,7];
+// arr[4] = [1,5,4,7,9];
 
-let sum = 0;
+// let sum = 0;
 
-for (let i = 0; i < arr.length; i++) {
-  if (i === 0 || i === arr.length - 1) {
-    for (let j = 0; j < arr[i].length; j++) {
-      sum += arr[i][j];
-    }
-  }
-}
+// for (let i = 0; i < arr.length; i++) {
+//   if (i === 0 || i === arr.length - 1) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       sum += arr[i][j];
+//     }
+//   }
+// }
 
-for (let i = 1; i < arr.length - 1; i++) {
-  sum += arr[i][0] + arr[i][arr[i].length - 1];
-}
-console.log(sum);
+// for (let i = 1; i < arr.length - 1; i++) {
+//   sum += arr[i][0] + arr[i][arr[i].length - 1];
+// }
+// console.log(sum);
 
 
 // Câu 2. tính tổng giá trị 2 tam giác trên
 // Câu 3. tính tổng các giá trị là sô chẵn
+
+// let arr = [];
+// arr[0] = [2,3,4,5,7];
+// arr[1] = [4,5,4,1,7];
+// arr[2] = [2,1,6,2,7];
+// arr[3] = [2,3,3,7,7];
+// arr[4] = [1,5,4,7,9];
+// let sum = 0;
+// let count = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   for (let j = 0; j < arr[i].length; j++) {
+//     if (arr[i][j] % 2 == 0 && count < 24) {
+//       sum += arr[i][j];
+//       count++;
+//     }
+//   }
+// }
+// console.log(sum);
 
 
 // Câu 4. tính tổng các giá trị là số nguyên tố
@@ -101,7 +119,7 @@ console.log(sum);
 
 // Câu 5. tính tổng 5 số chẵn đầu tiên trong mảng 2 chiều
 
-
+// let arr = [];
 // arr[0] = [2,3,4,5,7];
 // arr[1] = [4,5,4,1,7];
 // arr[2] = [2,1,6,2,7];
@@ -111,7 +129,7 @@ console.log(sum);
 // let count = 0;
 // for (let i = 0; i < arr.length; i++) {
 //   for (let j = 0; j < arr[i].length; j++) {
-//     if (arr[i][j] % 2 == 0 && count < 4) {
+//     if (arr[i][j] % 2 == 0 && count < 5) {
 //       sum += arr[i][j];
 //       count++;
 //     }
