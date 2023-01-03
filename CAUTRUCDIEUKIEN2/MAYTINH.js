e84948cc69b4cfa3861159bcd6485a0fdd8302f7
iChikael <120639771+iChikael@users.noreply.github.com>
2023-01-03 08:57:55 +0700

    BTT

--- CAUTRUCDIEUKIEN2/MAYTINH.js
+++ CAUTRUCDIEUKIEN2/MAYTINH.js
@@ -1,3 +1,16 @@
 var runningTotal=0;
 var buffer = "0";
 var previousOperator;
+
+
+const screen = document.querySelector('.screen');
+
+function buttonClick(value){
+    if(isNaN(value)){
+        handleSymbol(value);
+    }
+    else{
+        handleNumber(value);
+    }
+    screen.innerText=buffer
+}
